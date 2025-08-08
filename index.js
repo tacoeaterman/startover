eaf6156d4d1e20e034af4cc831e3184525dc9cb2
tacoeaterman <berkana84@gmail.com>
2025-08-07 21:12:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const { default: createServer } = require('./dist/index-prod.js');
 
 const app = express();
 
-// Serve static files
+// Serve static files from the built public directory
 app.use(express.static(path.join(__dirname, 'dist/public')));
 
 // API routes and server setup
 createServer(app);
 
 // Export the Express app as a Firebase Function
-exports.app = onRequest(app);
+exports.app = onRequest({
+  memory: '1GiB',
+  timeoutSeconds: 60
+}, app);
